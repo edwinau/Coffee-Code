71ac39a07e915748f3107efc9f6757d7f2545613
EDWIN AU <edwinau@rogers.com>
2017-04-06 09:44:13 -0400

    Coffee and Code 4 all 3 complete

--- CoffeeandCode4.rb
+++ CoffeeandCode4.rb
@@ -5,3 +5,26 @@ array.each do |num|
 end
 
 puts sum
+
+movies = [['Alfonso Cuaron', 'Gravity'], ['Spike Jonze', 'Her'], ['Martin Scorsese', 'The Wolf of Wall Street']]
+
+hash_movies = Hash[movies]
+
+hash ={}
+movies.each do |movies|
+  hash[movies[0]] = movie[1]
+end
+print hash_movies.inspect
+print hash.inspect
+
+
+
+numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+suits = ['spades', 'hearts', 'diamonds', 'clubs']
+#
+# combination = Hash.new[]
+# numbers.each do |nr|
+#   puts combination [:number => nr] = [:suits => suits]
+# end
+
+puts numbers.product(suits)

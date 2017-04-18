e58e297b91ebe72f8cb435749265284e03e677ef
EDWIN AU <edwinau@rogers.com>
2017-04-18 09:56:26 -0400

    answer 1 and 2

--- CoffeenCode7.rb
+++ CoffeenCode7.rb
@@ -3,7 +3,18 @@ song = [["row", 3], ["your", 1], ["boat", 1]]
 def songs(array)
   phrase = []
   array.each do |chunk|
-      phrase << (chunk[0]*chunk[1])
+    chunk[1].times do
+      phrase << chunk[0]
+    end
   end
   return phrase
+end
+
+def narcissistic(num)
+  sum = 0
+    num.to_s.length
+    num.to_s.each_char do |digit|
+      sum += digit.to_i * num.to_s.length
+      end
+    sum
 end
